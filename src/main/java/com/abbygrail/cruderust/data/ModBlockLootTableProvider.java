fb5437d0c37e94b779e0dcc019faca542faf59b5
Nary <65220410+nrtcb@users.noreply.github.com>
2025-05-27 11:12:28 +0330

    datagen part 1

---
+++ src/main/java/com/abbygrail/cruderust/data/ModBlockLootTableProvider.java
@@ -0,0 +1,32 @@
+package com.abbygrail.cruderust.data;
+
+import com.abbygrail.cruderust.cruderust;
+import net.minecraft.core.Holder;
+import net.minecraft.core.HolderLookup;
+import net.minecraft.data.loot.BlockLootSubProvider;
+import net.minecraft.world.flag.FeatureFlagSet;
+import net.minecraft.world.flag.FeatureFlags;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
+
+import java.util.Set;
+
+public class ModBlockLootTableProvider extends BlockLootSubProvider {
+    protected ModBlockLootTableProvider(HolderLookup.Provider registries) {
+        super(Set.of(), FeatureFlags.REGISTRY.allFlags(), registries);
+    }
+
+    @Override
+    protected void generate() {
+        dropSelf(cruderust.BRONZE_BLOCK.get());
+        createSlabItemTable(cruderust.FIREWOOD.get());
+
+
+
+    }
+
+    @Override
+    protected Iterable<Block> getKnownBlocks() {
+        return cruderust.BLOCKS.getEntries().stream().map(Holder::value)::iterator;
+    }
+}
---
+++ src/main/java/com/abbygrail/cruderust/data/ModItemModelprovider.java
@@ -0,0 +1,4 @@
+package com.abbygrail.cruderust.data;
+
+public class ModItemModelprovider {
+}
